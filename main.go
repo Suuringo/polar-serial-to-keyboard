4832dca3cdb7adbd38a222a85389a78fb129739b
Suuringo <72997519+Suuringo@users.noreply.github.com>
2021-12-17 01:39:56 +0100

    Use config to connect to serial device instead of cmd prompt

--- main.go
+++ main.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"polar/keyboard"
+	"strings"
 
 	"github.com/getlantern/systray"
 	"go.bug.st/serial"
+	"go.bug.st/serial/enumerator"
 )
+
+type Config struct {
+	Port       string `json:"port",omitempty`
+	DeviceName string `json:"deviceName",omitempty`
+}
 
 //go:embed shig.ico
 var shigLove []byte
 
 func main() {
 	systray.Run(onReady, onExit)
-	onReady()
+}
+
+func readConfig() (conf Config) {
+	buf, err := os.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal("Error opening config.json ", err)
+	}
+	err = json.Unmarshal(buf, &conf)
+	if err != nil {
+		log.Fatal("Error parsing config.json ", err)
+	}
+	return
 }
 
 func onReady() {
@@ -32,7 +51,30 @@ func onReady() {
 		fmt.Println("Finished quitting")
 	}()
 
-	portInfo := promptPort()
+	var portName string
+
+	conf := readConfig()
+	if conf.DeviceName != "" {
+		ports, err := enumerator.GetDetailedPortsList()
+		if err != nil {
+			log.Fatal("Error getting serial port list! ", err)
+		}
+		if len(ports) == 0 {
+			log.Fatal("No serial ports found! ")
+		}
+
+		for _, port := range ports {
+			if strings.HasPrefix(strings.ToLower(port.Product), strings.ToLower(conf.DeviceName)) {
+				portName = port.Name
+				break
+			}
+		}
+	} else if conf.Port != "" {
+		portName = conf.Port
+	} else {
+		log.Fatal("deviceName and port not specified in conf.json!")
+	}
+
 	mode := &serial.Mode{
 		BaudRate: 9600,
 		Parity:   serial.NoParity,
@@ -40,12 +82,12 @@ func onReady() {
 		StopBits: serial.OneStopBit,
 	}
 
-	port, err := serial.Open(portInfo.Name, mode)
+	port, err := serial.Open(portName, mode)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error opening serial port ", portName, err)
 	}
 
-	log.Println("Opened port", portInfo.Name)
+	log.Println("Opened port", portName)
 
 	listenString(port, LF, keyboard.SendString)
 }
